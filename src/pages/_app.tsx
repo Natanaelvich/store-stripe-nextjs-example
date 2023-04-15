795e594ad4790f4e1cf3a1089e60d44236b5dc35
Natanaelvich <taelima1997@gmail.com>
2023-04-14 22:48:35 -0300

    update home app with new styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import { AppProps } from 'next/app';
+import Image from 'next/image';
 import { globalStyles } from '../styles/global';
+import { Container, Header } from '../styles/pages/app';
+import logoImg from '../assets/logo.svg';
 
 globalStyles();
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
 
 export default App;
