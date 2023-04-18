c355841448361b646d0142b725883f1caddf7139
Natanaelvich <taelima1997@gmail.com>
2023-04-17 22:39:30 -0300

    add link to home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,17 @@ import Image from 'next/image';
 import { globalStyles } from '../styles/global';
 import { Container, Header } from '../styles/pages/app';
 import logoImg from '../assets/logo.svg';
+import Link from 'next/link';
 
 globalStyles();
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
-      {/* // TODO: link to home page */}
       <Header>
-        <Image src={logoImg} alt="" />
+        <Link href="/">
+          <Image src={logoImg} alt="" />
+        </Link>
       </Header>
 
       <Component {...pageProps} />
